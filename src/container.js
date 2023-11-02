86996b8e23e972e5524b02c32ae1a4c2f808dff6
jiyeonyoo <ji04230423@gmail.com>
2023-11-03 01:26:20 +0900

    feat: Make main layout

---
+++ src/container.js
@@ -0,0 +1,14 @@
+import './container.css';
+
+function Container() {
+    return (
+        <div className="container">
+            <div className="container-album_covers"></div>
+            <div className="container-main_slogan">
+
+            </div>
+        </div>
+    );
+}
+
+export default Container;
